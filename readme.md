e93ec29b106b60f8ddccb162da508527f9d4ce0a
atomzamonday <chayoot789@hotmail.com>
2021-10-28 16:20:17 +0700

    add readme and json support.

---
+++ readme.md
@@ -0,0 +1,3 @@
+0. first step => `yarn install`.
+1. dev => `yarn dev`.
+2. build => `yarn build`.
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { config } from "dotenv";
+import { greeting } from "../test.json";
 
 if (process.env["NODE_ENV"] !== "production") {
   config();

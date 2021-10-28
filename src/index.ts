ebab0a5d2c7e9f62cebba17650909f9ff45d9315
atomzamonday <chayoot789@hotmail.com>
2021-10-28 16:24:41 +0700

    fix json resolving.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { config } from "dotenv";
-import { greeting } from "../test.json";
+import { greeting } from "./test.json";
 
 if (process.env["NODE_ENV"] !== "production") {
   config();
 }
 
-console.log("hello world");
+console.log(greeting);
 console.log(process.env["DEV"]);
 
 export {};
